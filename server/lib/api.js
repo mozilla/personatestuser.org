0f32800d05e3cc424eb6f21f400ae0cb19269275
Jed Parsons <jedp@me.com>
2012-05-21 18:16:52 -0700

    implemented getTestUser

--- server/lib/api.js
+++ server/lib/api.js
@@ -1,2 +1,155 @@
-// personatestuser.org api
+const redis = require('redis'),
+      DEFAULT_DOMAIN = 'personatestuser.org';
+
+// All our redis keys are prefixed with 'ptu:'
+//
+// ptu:nextval = an iterator
+// ptu:emails = zset of user emails scored by creation date
+// ptu:<email> = password for user with given email
+var redisClient = redis.createClient();
+
+module.exports.getTestUser = function getTestUser(callback) {
+  // pick a unique username and assign a random password.
+  try {
+    var name = getRandomName();
+    var password = getRandomPassword();
+    var email;
+
+    redisClient.incr('ptu:nextval', function(err, val) {
+      email = name + val + '@' + DEFAULT_DOMAIN;
+
+      var multi = redisClient.multi();
+      multi.zadd('ptu:emails', (new Date()).getTime(), email);
+      multi.set('ptu:'+email, password);
+      multi.exec(function(err) {
+        if (err) return callback(err);
+        return callback(null, {
+          'email': email, 
+          'password': password
+        });
+      });
+    });
+
+  } catch (err) {
+    return callback(err);
+  }
+};
+
+module.exports.deleteTestUser = function deleteTestUser(email, callback) {
+  try {
+    var multi = redisClient.multi();
+    multi.del('ptu:'+email);
+    multi.zrem('ptu:emails', email);
+    multi.exec(callback);
+  } catch (err) {
+    return callback(err);
+  }
+};
+
+module.exports.getAssertion = function getAssertion(params, callback) {
+  try{
+    var email = params.email;
+    var password = params.password;
+    var audience = params.audience;
+    if (! email && password && audience) {
+      return callback(new Error("required param missing"));
+    }
+
+    return callback(null, {
+      'assertion': 'I like pie'
+    });
+  } catch (err) {
+    return callback(err);
+  }
+};
+
+// ----------------------------------------------------------------------
+// Utils for this module
+
+var names = [
+  'abel',
+  'abreu',
+  'acevedo',
+  'adrian',
+  'aguilera',
+  'alexis',
+  'andy',
+  'angelo',
+  'anthony',
+  'ashley',
+  'avellanet',
+  'blass',
+  'blazquez',
+  'cancel',
+  'carlos',
+  'cesar',
+  'charlie',
+  'daniel',
+  'didier',
+  'edward',
+  'farrait',
+  'fernando',
+  'galindo',
+  'garcia',
+  'gomez',
+  'grullon',
+  'hernandez',
+  'johnny',
+  'jonathan',
+  'lopez',
+  'lozada',
+  'martin',
+  'masso',
+  'melendez',
+  'miguel',
+  'montenegro',
+  'nefty',
+  'olivares',
+  'oscar',
+  'ralphy',
+  'rawy',
+  'ray',
+  'raymond',
+  'rene',
+  'reyes',
+  'ricky',
+  'robert',
+  'robi',
+  'rodriguez',
+  'rosa',
+  'rossello',
+  'roy',
+  'ruben',
+  'ruiz',
+  'sallaberry',
+  'serbia',
+  'sergio',
+  'talamantez',
+  'torres',
+  'weider',
+  'xavier'];
+
+var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' +
+            'abcdefghijklmnopqrstuvwxyz' +
+            '1234567890' +
+            '~#$%^&*(){}[]_+-=,.;: ';
+
+var numNames = names.length;
+var numChars = chars.length;
+
+function getRandomName() {
+  return names[Math.floor(Math.random() * numNames)];
+}
+
+function getRandomPassword(length) {
+  length = length || 16;
+
+  var i,
+      password = '';
+
+  for (i = 0; i < length; i++) {
+    password += chars[Math.floor(Math.random() * numChars)];
+  }
+  return password;
+}
 
--- test/api_vows.js
+++ test/api_vows.js
@@ -1,15 +1,40 @@
-var vows = require("vows");
-var assert = require("assert");
+const vows = require("vows"),
+      assert = require("assert"),
+      api = require("../server/lib/api"),
+      redis = require('redis');
 
 vows.describe("API vows")
 
-.addBatch({
-  "Test suites": {
-    topic: 42, 
+// XXX spin up a test redis for this
 
-    "are functional": function(meaning) { 
-      assert(meaning === 42);
-    }
+.addBatch({
+  "getTestUser": {
+    topic: function() {
+      api.getTestUser(this.callback);
+    },
+
+    "gives us a valid email and password": function(data) {
+      assert(data.email.indexOf('@') > 0);
+      assert(data.password.length >= 16);
+    }, 
+    
+    "stores the email and password": { 
+      topic: function(data) {
+        var multi = redis.createClient().multi();
+        var cb = this.callback;
+        multi.zrank('ptu:emails', data.email);
+        multi.get('ptu:'+data.email);
+        multi.exec(function(err, results) {
+          return cb(err, data, results);
+        });   
+      },
+
+      "in redis": function(err, initialData, redisResults) {
+        assert(err === null);
+        assert(redisResults[0] >= 0);
+        assert(redisResults[1] === initialData.password);
+      }
+    },
   }
 })
 
