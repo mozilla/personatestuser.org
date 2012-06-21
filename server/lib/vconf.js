ac1ab0c836dfb6380e784956005230135c37c6cd
Jed Parsons <jedp@me.com>
2012-06-21 14:20:58 -0700

    added vconf module

---
+++ server/lib/vconf.js
@@ -0,0 +1,14 @@
+module.exports = {
+  prod: {
+    browserid: 'https://browserid.org',
+    verifier: "https://browserid.org/verify"
+  },
+  stage: {
+    browserid: 'https://diresworb.org',
+    verifier: "https://diresworb.org/verify"
+  },
+  dev: {
+    browserid: 'https://login.dev.anosrep.org',
+    verifier: "https://verifier.dev.anosrep.org"
+  }
+};
